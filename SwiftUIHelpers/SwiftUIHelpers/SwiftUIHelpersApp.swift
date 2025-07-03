3e2bc656c6a9d87cfbdb88767c87c2c6db817815
Alysson Menezes Dodo <almenezes912@gmail.com>
2025-07-03 10:54:02 -0300

    start new Project

---
+++ SwiftUIHelpers/SwiftUIHelpers/SwiftUIHelpersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIHelpersApp.swift
+//  SwiftUIHelpers
+//
+//  Created by ALYSSON MENEZES on 03/07/25.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIHelpersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
